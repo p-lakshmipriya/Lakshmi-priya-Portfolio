2cc20df3494ecdc08360bd6f0a49263f5bb5760e
priyalk2023 <153483940+priyalk2023@users.noreply.github.com>
2024-04-03 13:18:58 +0530

    Add the portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+var menuBtn = document.querySelector(".menu-btn");
+var menu = document.querySelector(".nav-links");
+var menuLinks = document.querySelectorAll(".nav-links li a");
+
+menuBtn.addEventListener("click", activeClass);
+
+function activeClass() {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+for (i = 0; i < menuLinks.clientHeight; i++) {
+  menuLinks[i].addEventListener("click", menuItemClicked);
+}
+function menuItemClicked() {
+  menuBtn.classList.remove("active");
+  menu.classList.remove("active");
+}
+var homeSection = document.querySelector(".home");
+window.addEventListener("scroll", scrollFunction);
+window.addEventListener("load", scrollFunction);
+
+function scrollFunction() {
+  if (window.scrollY > 60) {
+    homeSection.classList.add("active");
+  } else {
+    homeSection.classList.remove("active");
+  }
+}
+// home section end
+
+// portfolio Start
+// var $galleryContainer = $(".gallery").isotope({
+//   itemSelector: ".item",
+//   layoutMode: "ffitRows",
+// });
+// $(".button-group .button").on("click", function () {
+//   $(".button-group .button").removeClass("active");
+//   $(this).addClass("active");
+
+//   var value = $(this).attr("data-filter");
+//   $galleryContainer.isotope({
+//     filter: value,
+//   });
+// });
+
+// // magnific popup
+
+// $(".gallery").magnificPopup({
+//   delegate: ".overlay a",
+//   type: "image",
+//   gallery: {
+//     enabled: true,
+//   },
+// });
